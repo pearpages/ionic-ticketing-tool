dff63adbefd8ede3e27510a70fe0d8e6ebbbcab0
pearpages <pere@soms.cat>
2015-12-16 12:50:45 +0100

    logging error message

--- www/js/account/account.controller.js
+++ www/js/account/account.controller.js
@@ -2,15 +2,16 @@
 	'use strict';
 
 	angular.module("ticketing-account")
-	.controller('AccountController',['$state','myUsers','myTickets','myTicketsMocks',AccountController]);
+	.controller('AccountController',['$state','myUsers','myTickets','myTicketsMocks','$ionicLoading','$timeout',AccountController]);
 
-	function AccountController($state,myUsers,myTickets,myTicketsMocks) {
+	function AccountController($state,myUsers,myTickets,myTicketsMocks,$ionicLoading,$timeout) {
 
 		var vm = this;
 
 		vm.user = null;
 		vm.form = null;
 		vm.validateUser = validateUser;
+		vm.hideErrorMessage = hideErrorMessage;
 		vm.logout = logout;
 		vm.mock = mock;
 
@@ -21,16 +22,40 @@
 			vm.form = {};
 			vm.form.userid = null;
 			vm.form.password = null;
+			vm.form.message = null;
 			vm.user = myUsers.getCurrentUser();
+		}
+
+		function hideErrorMessage() {
+			vm.form.message = null;
 		}
 
 		function mock() {
 			myTicketsMocks.mockTickets(50,myTickets,vm.user.id); 
 		}
 
+		function showLoading() {
+			$ionicLoading.show({
+				template: 'Validating...'
+			});
+		}
+
+		function hideLoading() {
+			$timeout(function() {
+				vm.user = myUsers.validateUser(vm.form.userid,vm.form.password);
+				if(vm.user === null){
+					vm.form.message = 'Wrong password';
+					console.log(vm.user,vm.form.message);
+				}else{
+					$state.reload();
+				}
+				$ionicLoading.hide();				
+			},1000);
+		}
+
 		function validateUser() {
-			vm.user = myUsers.validateUser(vm.form.userid,vm.form.password);
-			$state.reload();
+			showLoading();
+			hideLoading();
 		}
 
 		function logout() {
