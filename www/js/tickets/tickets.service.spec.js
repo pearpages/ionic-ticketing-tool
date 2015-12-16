57cc7b8323f25b551d187464cfee3f44494a9a58
pearpages <pere@soms.cat>
2015-12-16 18:06:26 +0100

    unit testing setup

---
+++ www/js/tickets/tickets.service.spec.js
@@ -0,0 +1,43 @@
+describe('tickets service',function () {
+
+    var myTickets;
+
+    beforeEach(module('my-tickets'));
+
+    beforeEach(inject(function(_myTickets_) {
+        myTickets = _myTickets_;
+    }));
+
+    it('should make an empty ticket', function() {
+
+    });
+        
+    it('should save a ticket to the struct of tickets', function() {
+        
+    });
+    
+    it('should find a ticket in the struct of tickets', function() {
+        
+    });
+
+    it('should get the tickets for a given userid', function() {
+        
+    });
+
+    it('should give the size of the ticket struct', function() {
+        
+    });
+
+    it('should give an array with all the tickets not yet assigned', function() {
+        
+    });
+
+    it('should give an array with all the tickets closed', function() {
+        
+    });
+            
+    it('should give an array witn all the tickets of the current user logged', function() {
+                    
+    });            
+
+});
