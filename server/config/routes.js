cd0aa918d92e6f3a86f10ec4ded0b50d8bfd9ac1
pearpages <pere@soms.cat>
2016-01-07 11:03:17 +0100

    server minor

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,11 @@
 module.exports = function(app) {
 
+    app.get('/ticket/:id', function(req,res) {
+        res.send(req.id);
+    });
+
     app.get('*', function(req, res) {
-        res.send('success');
+        res.status(404);
+        res.send('Page does not exist');
     });
 };
