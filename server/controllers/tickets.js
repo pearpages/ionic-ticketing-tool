86c5a8895b1a4f129a8f25b3353914f5c93fc3c6
pearpages <pere@soms.cat>
2016-01-07 18:07:27 +0100

    server side ticket model

--- server/controllers/tickets.js
+++ server/controllers/tickets.js
@@ -1,6 +1,8 @@
+var Ticket = require('../models/Ticket');
+
 exports.getTickets = function(req, res) {
     res.send(req.params);
 };
 exports.getTicket = function(req, res) {
-    res.send(req.params.id);
+    res.send(Ticket.getTicket(req.params.id));
 }
--- server/models/Ticket.js
+++ server/models/Ticket.js
@@ -1,79 +1,83 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-module.exports = function() {
-    var commentSchema = new Schema({
-        comment: String,
-        date: {
-            type: Date,
-            default: Date.now
-        }
-    });
+var commentSchema = new Schema({
+    comment: String,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
 
-    var ticketSchema = new Schema({
-        express: {
-            type: Boolean,
-            default: false
-        }
-        status: {
-            type: String,
-            default: 'open'
-        }
-        notified: {
-            type: Date,
-            default: Date.now
-        }
-        who: {
-            type: String,
-            required: true
-        },
-        issue: {
-            type: Number,
-            required: true
-        },
-        issueDescription: {
-            //description copied from the issue object
-            type: String,
-            required: true
-        },
-        description: {
-            type: String
-        }
-        closed: {
-            type: Date
-        }
-        evaluation: Number,
-        image: String,
-        comments: [commentSchema]
-        requested: {
-            type: String,
-            required: true
-        }
-        it: String,
-        office: String //office comes given by the who 'user'
-    });
+var ticketSchema = new Schema({
+    express: {
+        type: Boolean,
+        default: false
+    }
+    status: {
+        type: String,
+        default: 'open'
+    }
+    notified: {
+        type: Date,
+        default: Date.now
+    }
+    who: {
+        type: String,
+        required: true
+    },
+    issue: {
+        type: Number,
+        required: true
+    },
+    issueDescription: {
+        //description copied from the issue object
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    }
+    closed: {
+        type: Date
+    }
+    evaluation: Number,
+    image: String,
+    comments: [commentSchema]
+    requested: {
+        type: String,
+        required: true
+    }
+    it: String,
+    office: String //office comes given by the who 'user'
+});
 
-    ticketSchema.methods.isCompleted = function() {
-        var res;
-        if (this.who !== null && this.issue > -1) {
-            return res = true;
-        } else {
-            return res = false;
-        }
-        return res;
-    };
+ticketSchema.methods.isCompleted = function() {
+    var res;
+    if (this.who !== null && this.issue > -1) {
+        return res = true;
+    } else {
+        return res = false;
+    }
+    return res;
+};
 
-    ticketSchema.methods.close = function() {
-        this.status = 'closed';
-        this.closed = new Date();
-    };
+ticketSchema.methods.close = function() {
+    this.status = 'closed';
+    this.closed = new Date();
+};
 
-    ticketSchema.methods.addComment = function(text) {
-        var std = {};
-        std.comment = text;
-        std.date = new Date();
-        this.comments.push(std);
-    };
+ticketSchema.methods.addComment = function(text) {
+    var std = {};
+    std.comment = text;
+    std.date = new Date();
+    this.comments.push(std);
+};
 
-    return mongoose.model('Ticket', ticketSchema);
-}();
+var Ticket = mongoose.model('Ticket',ticketSchema);
+
+exports.Ticket = Ticket;
+
+exports.getTicket = function(ticketid) {
+    return Ticket.find({_id: ticketid});
+}
